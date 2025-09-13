3f8182521ee7a4272d0d73f75e1eee918b4ccb93
Jiya Dua <jiya.dua.27@gmail.com>
2025-09-13 09:10:51 +0000

    do what you want

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -11,51 +11,18 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     >
       <rect width="256" height="256" fill="none" />
       <path
-        d="M128,56v40a16,16,0,0,1-16-16,16.3,16.3,0,0,0-16-16,16,16,0,0,0,0,32,48,48,0,0,1,48,48v40"
+        d="M176,40H80A56,56,0,0,0,24,96v2.5C24,150,72.5,216,128,216s104-66,104-117.5V96A56,56,0,0,0,176,40Z"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="24"
       />
-      <path
-        d="M128,96a16,16,0,0,1,16-16,16.3,16.3,0,0,1,16-16,16,16,0,0,1,0,32,16,16,0,0,0-16,16"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      />
-      <path
-        d="M216,128a88,88,0,1,1-88-88"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      />
-      <line
-        x1="88"
-        y1="128"
-        x2="168"
-        y2="128"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      />
-      <line
-        x1="128"
-        y1="88"
-        x2="128"
-        y2="168"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      />
+      <circle cx="128" cy="128" r="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+      <line x1="128" y1="108" x2="128" y2="80" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+      <line x1="128" y1="176" x2="128" y2="148" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+      <line x1="108" y1="128" x2="80" y2="128" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+      <line x1="176" y1="128" x2="148" y2="128" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
     </svg>
   );
 }

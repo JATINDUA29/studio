9452bdbdcf04ead34f7afd23f5e82f1cf32ab2d9
Jiya Dua <jiya.dua.27@gmail.com>
2025-09-13 09:05:54 +0000

    can you change the logo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -11,18 +11,23 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     >
       <rect width="256" height="256" fill="none" />
       <path
-        d="M168,40.7a88,88,0,1,1-80,0"
+        d="M128,56v40a16,16,0,0,1-16-16,16.3,16.3,0,0,0-16-16,16,16,0,0,0,0,32,48,48,0,0,1,48,48v40"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="24"
       />
-      <line
-        x1="128"
-        y1="136"
-        x2="128"
-        y2="240"
+      <path
+        d="M128,96a16,16,0,0,1,16-16,16.3,16.3,0,0,1,16-16,16,16,0,0,1,0,32,16,16,0,0,0-16,16"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="24"
+      />
+      <path
+        d="M216,128a88,88,0,1,1-88-88"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
@@ -31,9 +36,20 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       />
       <line
         x1="88"
-        y1="176"
+        y1="128"
         x2="168"
-        y2="176"
+        y2="128"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="24"
+      />
+      <line
+        x1="128"
+        y1="88"
+        x2="128"
+        y2="168"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"

e92bae4f8f30c4ccb21bf0b073e0c6de9c672ab1
Jiya Dua <jiya.dua.27@gmail.com>
2025-09-13 09:25:26 +0000

    check the error

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -11,18 +11,29 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     >
       <rect width="256" height="256" fill="none" />
       <path
-        d="M176,40H80A56,56,0,0,0,24,96v2.5C24,150,72.5,216,128,216s104-66,104-117.5V96A56,56,0,0,0,176,40Z"
+        d="M49.3,135.3,77.9,132a8,8,0,0,1,8,8.8l-3.3,26.2a8,8,0,0,0,8,8.8l23.4-2.9a8,8,0,0,1,8,8.8l-3.3,26.2a8,8,0,0,0,8,8.8l32-4a8,8,0,0,1,8,8.8l-3.3,26.2a8,8,0,0,0,8.8,8L225.3,248"
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="24"
       />
-      <circle cx="128" cy="128" r="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
-      <line x1="128" y1="108" x2="128" y2="80" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
-      <line x1="128" y1="176" x2="128" y2="148" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
-      <line x1="108" y1="128" x2="80" y2="128" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
-      <line x1="176" y1="128" x2="148" y2="128" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="24"/>
+      <path
+        d="M208,40H48A16,16,0,0,0,32,56V200a16,16,0,0,0,16,16H184"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="24"
+      />
+      <polyline
+        points="80 104 112 104 128 80 144 128 176 128"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="24"
+      />
     </svg>
   );
 }
